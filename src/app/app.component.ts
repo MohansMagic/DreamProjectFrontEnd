0827109edc91bc9a29d1b9af060fdb2c076d7ca3
MohansMagic <139035274+MohansMagic@users.noreply.github.com>
2024-08-07 19:18:55 +0530

    Update app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { AboutComponent } from "./about/about.component";
+import { CommonModule } from '@angular/common';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.css',
+    imports: [RouterOutlet, AboutComponent,CommonModule,HttpClientModule]
 })
 export class AppComponent {
-  title = 'DreamProjectfrontend';
+  title = 'DreamProject';
+  data: any;
+
+
+  constructor(private http: HttpClient) {}
+
+  fetchData() {
+    this.http.get('https://yoga-10086244327.development.catalystappsail.com/asanaapi/getasanas')
+      .subscribe(response => {
+        this.data = response;
+        console.log(this.data);
+      }, error => {
+        console.error('Error fetching data:', error);
+      });
+  }
+
 }
