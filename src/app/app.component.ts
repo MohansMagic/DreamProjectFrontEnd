4da6fc1e2151199e40f55725c6d840b58104df80
MohansMagic <mohankumarpalanisamy235@gmail.com>
2024-08-11 20:52:53 +0530

    added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -155,6 +155,14 @@ export class AppComponent {
       abc: '6',
       def: 'Sirasana'
     };
+
+    const obj = { hello: "world" };
+const blob = new Blob([JSON.stringify(obj, null, 2)], {
+  type: "application/json",
+}); 
+
+console.log('blob! is ', payload,{blob});
+
       const headers=new HttpHeaders({
         'Content-Type': 'application/json'
         });     
