ce6954870439ca410ba33b3f4eeb85be2f302609
MohansMagic <mohankumarpalanisamy235@gmail.com>
2024-10-10 20:13:18 +0530

    Adding fetchasanas logic and also added forms logic

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,20 +1,53 @@
 import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, HttpClientModule],
+  imports: [CommonModule, HttpClientModule,FormsModule],
   templateUrl: './app.component.html'
 })
 export class AppComponent {
   private http = inject(HttpClient);
-  response: any;
+  healthresponse: any;
+  addyogasanaresponse: any;
   abc!: String;
+
+  asanaid!: number;
+  asananame!: string;
+  url!: string;
+  asanas: any[] = [];
+
+
+  FetchAllasanas() {
+    this.http.get<any>('https://10octyoga-10089526778.development.catalystappsail.com/Yogasana/AllYogasana').subscribe(
+      res => console.log('Health received from backend for the urlis ',this.asanas=res),
+      err => console.error('Error:', err)
+    );
+  }
+
+  submitForm() {
+    const newAsana = {
+      asanaid: this.asanaid,
+      asananame: this.asananame,
+      url: this.url,
+    };
+
+    const headers = new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin':'https://asanasapp.netlify.app'
+    });
+    
+    this.http.post<any>('https://10octyoga-10089526778.development.catalystappsail.com/Yogasana/AddYogasana',newAsana,{headers}).
+    subscribe(res => console.log('addyogasanaresponse response received from backend for addyogasana end point is',this.addyogasanaresponse=res)
+    );
+  }
+  
   BackEndApiHealth() {
-    this.response="";
+    this.healthresponse="";
     this.http.get<any>('https://10octyoga-10089526778.development.catalystappsail.com/health').subscribe(
-      res => console.log('response',this.response=res),
+      res => console.log('Health received from backend for the url https://10octyoga-10089526778.development.catalystappsail.com/health is ',this.healthresponse=res),
       err => console.error('Error:', err)
     );
   }
@@ -31,7 +64,7 @@ export class AppComponent {
     });
     
     this.http.post<any>('https://10octyoga-10089526778.development.catalystappsail.com/Yogasana/AddYogasana',body,{headers}).
-    subscribe(res => console.log('response',this.response=res)
+    subscribe(res => console.log('addyogasanaresponse response received from backend for addyogasana end point is',this.addyogasanaresponse=res)
     );
   }
 }
